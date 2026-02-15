cae00272a3645cad38ca62111843490908d6059f
PeterWanjihia <peter.wanjihia@student.moringaschool.com>
2026-02-15 14:09:18 +0300

    designing the contract of the tool

--- projects/01_filesystem_explorer/run.py
+++ projects/01_filesystem_explorer/run.py
@@ -3,6 +3,9 @@ from pathlib import Path
 def list_directory(path:str):
     root = Path(path)
     for p in  sorted(root.iterdir()):
-        print(p.name)
+        return [p.name for p in sorted(root.iterdir())]
+    
+files = list_directory("/home/moringa/AgentCompute")
+print(files)
 
 
